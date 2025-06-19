8cc99fd378f364ce444f95afebaca2b361308e19
Alejandro <10653@holbertonstudents.com>
2025-06-19 12:13:52 +0000

    last example

--- Ejercicios/solitareNumber.py
+++ Ejercicios/solitareNumber.py
@@ -17,10 +17,23 @@ numero_solitario([4, 2, 3, 2, 4])  # Debe retornar 3
 
 
 def numero_solitario(lista):
-    contador{}
+    contador = {}
 
     for num in lista:
         if num is contador:
             contador[num] += 1
         else:
             contador[num] = 1
+
+    for num, cantidad in contador.items():
+        if cantidad == 1:
+            return num
+
+# Código para probar la función
+if __name__ == "__main__":
+    # Ejemplo del enunciado
+    print(numero_solitario([4, 2, 3, 2, 4]))  # Debe retornar 3
+    
+    # Pruebas adicionales
+    print(numero_solitario([1, 1, 2, 2, 3]))  # Debe retornar 3
+    print(numero_solitario([7, 5, 5, 7, 9]))  # Debe retornar 9
